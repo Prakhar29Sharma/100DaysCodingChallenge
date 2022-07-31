cd3dbc9f447a14fb125c42177c7e0ff5bfb98535
Prakhar29Sharma <prakhar29sharma@gmail.com>
2022-07-31 11:36:34 +0530

    basic if-else program

---
+++ IfElseExample.java
@@ -0,0 +1,26 @@
+public class IfElseExample{
+	public static void main(String[] args)
+	{
+		int n;
+		n = 20;
+		if(n > 10)
+		{
+			System.out.println("n is greater than 10!");
+		}
+		n = n / 2;
+		if(n == 10)
+		{
+			System.out.println("now n is equal to 10!");
+		}
+
+		n = n + 3;
+
+		if (n < 10)
+		{
+			System.out.println("will this be executed?");
+		}
+
+		return;
+		
+	}
+}
