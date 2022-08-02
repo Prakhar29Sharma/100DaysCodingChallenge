6db17f84f90cda755b264756f220254ad4a6b580
Prakhar Sharma <prakhar29sharma@gmail.com>
2022-08-02 07:14:00 +0530

    CharTest.java

---
+++ CharTest.java
@@ -0,0 +1,9 @@
+public class CharTest {
+    public static void main(String[] args){
+        char ch;
+        for(ch = 65; ch <= 'Z'; ch++)
+        {
+            System.out.println("" + ch);
+        }
+    }
+}
