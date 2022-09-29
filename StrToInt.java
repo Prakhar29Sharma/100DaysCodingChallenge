b870d29d8a19b4db866445488965d6f0bfdaae08
Prakhar Sharma <prakhar29sharma@gmail.com>
2022-09-29 22:31:10 +0530

    Java program to convert a String into Int

---
+++ StrToInt.java
@@ -0,0 +1,11 @@
+public class StrToInt {
+	public static void main(String args[]) {
+		int num1, num2;
+		String numInStr1 = "20";
+		String numInStr2 = "9";
+		num1 = Integer.parseInt(numInStr1);
+		num2 = Integer.parseInt(numInStr2);
+		System.out.println("sum : " +(num1+num2));
+		return;
+	}
+}
